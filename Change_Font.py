6a8384d5662461084adc4072a76ef10bc03ccea7
noobj2 <63994740+noobj2@users.noreply.github.com>
2021-08-27 05:00:26 +0430

    Update Change_Font.py

--- Change_Font.py
+++ Change_Font.py
@@ -1,6 +1,5 @@
 #// auth_ Mohamad Janati
-#// AmirHassan Asvadi ;)
-#// Copyright (c) 2020 Mohamad Janati (freaking stupid, right? :|)
+#// Copyright (c) 2020-2021 Mohamad Janati (freaking stupid, right? :|)
 
 from os.path import dirname
 from aqt.webview import AnkiWebView
@@ -11,6 +10,11 @@ from aqt import gui_hooks
 from aqt.qt import *
 from aqt.theme import theme_manager
 from aqt.utils import showInfo
+from anki import version
+import anki
+from anki.lang import is_rtl
+from anki.utils import isLin, isMac, isWin
+anki_version = int(version.replace('.', ''))
 
 config = mw.addonManager.getConfig(__name__)
 
@@ -94,7 +98,7 @@ class FontDialog(QDialog):
         mw.addonManager.writeConfig(__name__, conf)
         showInfo("Changes will take effect after you restart anki.", title="Anki [Change Font]")
 
-def stdHtml(
+def stdHtml_new(
     self,
     body: str,
     css: Optional[List[str]] = None,
@@ -129,7 +133,7 @@ def stdHtml(
         fontspec = 'font-size:{}px; font-family:"{}";'.format(font_size, family)
         widgetspec = """
         button { -webkit-appearance: none; background: #fff; border: 1px solid #ccc;
-        border-radius:5px; font-family: Helvetica }"""
+        border-radius:5px; font-family: %s }""" % family
     else:
         # family = self.font().family()
         color_hl_txt = palette.color(QPalette.HighlightedText).name()
@@ -190,6 +194,192 @@ def stdHtml(
     # print(html)
     self.setHtml(html)
 
+def stdHtml_old(self, body, css=None, js=None, head=""):
+    if css is None:
+        css = []
+    if js is None:
+        js = ["jquery.js"]
+
+    palette = self.style().standardPalette()
+    color_hl = palette.color(QPalette.Highlight).name()
+
+    family = config["Interface Font"]
+    font_size = config["Font Size"]
+
+    if isWin:
+        #T: include a font for your language on Windows, eg: "Segoe UI", "MS Mincho"
+        # family = _('"Segoe UI"')
+        widgetspec = "button { font-size: 12px; font-family:%s; }" % family
+        widgetspec += "\n:focus { outline: 1px solid %s; }" % color_hl
+        fontspec = 'font-size:{}px; font-family:"{}";'.format(font_size, family)
+    elif isMac:
+        # family="Helvetica"
+        fontspec = 'font-size:{}px; font-family:"{}";'.format(font_size, family)
+        widgetspec = """
+    button { font-size: 13px; -webkit-appearance: none; background: #fff; border: 1px solid #ccc;
+    border-radius:5px; font-family: %s }""" % family
+    else:
+        # family = self.font().family()
+        color_hl_txt = palette.color(QPalette.HighlightedText).name()
+        color_btn = palette.color(QPalette.Button).name()
+        fontspec = 'font-size:{}px; font-family:"{}";'.format(font_size, family)
+        widgetspec = """
+    /* Buttons */
+    button{ font-size:14px; -webkit-appearance:none; outline:0;
+        background-color: %(color_btn)s; border:1px solid rgba(0,0,0,.2);
+        border-radius:2px; height:24px; font-family:"%(family)s"; }
+    button:focus{ border-color: %(color_hl)s }
+    button:hover{ background-color:#fff }
+    button:active, button:active:hover { background-color: %(color_hl)s; color: %(color_hl_txt)s;}
+    /* Input field focus outline */
+    textarea:focus, input:focus, input[type]:focus, .uneditable-input:focus,
+    div[contenteditable="true"]:focus {
+    outline: 0 none;
+    border-color: %(color_hl)s;
+    }""" % {"family": family, "color_btn": color_btn,
+        "color_hl": color_hl, "color_hl_txt": color_hl_txt}
+
+    csstxt = "\n".join([self.bundledCSS("webview.css")]+
+                       [self.bundledCSS(fname) for fname in css])
+    jstxt = "\n".join([self.bundledScript("webview.js")]+
+                      [self.bundledScript(fname) for fname in js])
+    from aqt import mw
+    head =  mw.baseHTML() + head + csstxt + jstxt
+
+    html = """
+    <!doctype html>
+    <html><head>
+    <title>{}</title>
+
+    <style>
+    body {{ zoom: {}; background: {}; {} }}
+    {}
+    </style>
+
+    {}
+    </head>
+
+    <body>{}</body>
+    </html>""".format(self.title, self.zoomFactor(), self._getWindowColor().name(),
+                  fontspec, widgetspec, head, body)
+    #print(html)
+    self.setHtml(html)
+
+
+def standard_css_old(self) -> str:
+    palette = self.style().standardPalette()
+    color_hl = palette.color(QPalette.Highlight).name()
+
+    family = config["Interface Font"]
+    font_size = config["Font Size"]
+
+    if isWin:
+        # T: include a font for your language on Windows, eg: "Segoe UI", "MS Mincho"
+        button_style = "button { font-size: 12px; font-family:%s; }" % family
+        button_style += "\n:focus { outline: 1px solid %s; }" % color_hl
+        font = "font-size:{}px;font-family:{};".format(font_size, family)
+    elif isMac:
+        font = 'font-size:{}px;font-family:"{}";'.format(font_size, family)
+        button_style = """
+button { -webkit-appearance: none; background: #fff; border: 1px solid #ccc;
+border-radius:5px; font-family: %s }""" % family
+    else:
+        color_hl_txt = palette.color(QPalette.HighlightedText).name()
+        color_btn = palette.color(QPalette.Button).name()
+        font = 'font-size:{}px;font-family:"{}";'.format(font_size, family)
+        button_style = """
+/* Buttons */
+button{
+    background-color: %(color_btn)s;
+    font-family:"%(family)s"; }
+button:focus{ border-color: %(color_hl)s }
+button:active, button:active:hover { background-color: %(color_hl)s; color: %(color_hl_txt)s;}
+/* Input field focus outline */
+textarea:focus, input:focus, input[type]:focus, .uneditable-input:focus,
+div[contenteditable="true"]:focus {
+outline: 0 none;
+border-color: %(color_hl)s;
+}""" % {
+            "family": family,
+            "color_btn": color_btn,
+            "color_hl": color_hl,
+            "color_hl_txt": color_hl_txt,
+        }
+
+    zoom = self.zoomFactor()
+    background = self._getWindowColor().name()
+
+    if is_rtl(anki.lang.currentLang):
+        lang_dir = "rtl"
+    else:
+        lang_dir = "ltr"
+
+    return f"""
+body {{ zoom: {zoom}; background: {background}; direction: {lang_dir}; {font} }}
+{button_style}
+:root {{ --window-bg: {background} }}
+:root[class*=night-mode] {{ --window-bg: {background} }}
+"""
+
+def standard_css_new(self) -> str:
+    palette = theme_manager.default_palette
+    color_hl = palette.color(QPalette.Highlight).name()
+
+    family = config["Interface Font"]
+    font_size = config["Font Size"]
+
+    if isWin:
+        # T: include a font for your language on Windows, eg: "Segoe UI", "MS Mincho"
+        button_style = "button { font-size: 12px; font-family:%s; }" % family
+        button_style += "\n:focus { outline: 1px solid %s; }" % color_hl
+        font = f"font-size:{font_size}px;font-family:{family};"
+    elif isMac:
+        font = f'font-size:{font_size}px;font-family:"{family}";'
+        button_style = """
+button { -webkit-appearance: none; background: #fff; border: 1px solid #ccc;
+border-radius:5px; font-family: %s }""" % family
+    else:
+        color_hl_txt = palette.color(QPalette.HighlightedText).name()
+        color_btn = palette.color(QPalette.Button).name()
+        font = f'font-size:{font_size}px;font-family:"{family}";'
+        button_style = """
+/* Buttons */
+button{
+    background-color: %(color_btn)s;
+    font-family:"%(family)s"; }
+button:focus{ border-color: %(color_hl)s }
+button:active, button:active:hover { background-color: %(color_hl)s; color: %(color_hl_txt)s;}
+/* Input field focus outline */
+textarea:focus, input:focus, input[type]:focus, .uneditable-input:focus,
+div[contenteditable="true"]:focus {
+outline: 0 none;
+border-color: %(color_hl)s;
+}""" % {
+            "family": family,
+            "color_btn": color_btn,
+            "color_hl": color_hl,
+            "color_hl_txt": color_hl_txt,
+        }
+
+    zoom = self.zoomFactor()
+
+    window_bg_day = self.get_window_bg_color(False).name()
+    window_bg_night = self.get_window_bg_color(True).name()
+    body_bg = window_bg_night if theme_manager.night_mode else window_bg_day
+
+    if is_rtl(anki.lang.currentLang):
+        lang_dir = "rtl"
+    else:
+        lang_dir = "ltr"
+
+    return f"""
+body {{ zoom: {zoom}; background-color: {body_bg}; direction: {lang_dir}; }}
+html {{ {font} }}
+{button_style}
+:root {{ --window-bg: {window_bg_day} }}
+:root[class*=night-mode] {{ --window-bg: {window_bg_night} }}
+"""
+
 def open_window():
     font_dialog = FontDialog()
     font_dialog.exec()
@@ -198,5 +388,11 @@ action = QAction("Change Interface &Font", mw)
 action.triggered.connect(open_window)
 mw.form.menuTools.addAction(action)
 mw.addonManager.setConfigAction(__name__, open_window)
-
-AnkiWebView.stdHtml = stdHtml
+if anki_version < 2122:
+    AnkiWebView.stdHtml = stdHtml_old
+elif 2121 < anki_version < 2136:
+    AnkiWebView.stdHtml = stdHtml_new
+elif 2135 < anki_version < 2145:
+    AnkiWebView.standard_css = standard_css_old
+else:
+    AnkiWebView.standard_css = standard_css_new
